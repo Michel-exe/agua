d2b316b8e443ad812c12c6b9b9817f7d584c64b0
Michel <michl521521@gmail.com>
2022-08-12 21:29:51 -0500

    Actualizacion de links

--- code.js
+++ code.js
@@ -4,21 +4,29 @@ document.forms[0][1].addEventListener("keyup",(e)=>styleInput(e.target.parentEle
 document.forms[0][0].addEventListener("blur",(e)=>styleInput(e.target.parentElement,0))
 document.forms[0][1].addEventListener("blur",(e)=>styleInput(e.target.parentElement,0))
 
-let a=10
-let time= setInterval(()=>{
-    document.querySelector("#charge i").style.width=`${a}%`
-    a=a+10;
-}, 100);
+document.forms[0].addEventListener("click", e=>{
+    console.log("hola");
+    e.preventDefault()
+    document.querySelector(".admin").style.transform=`scale(1)`
+    let a=10
+    let time= setInterval(()=>{
+        document.querySelector("#charge i").style.width=`${a}%`
+        a=a+10;
+    }, 100);
+    
+    
+    setTimeout(() => {
+        clearInterval(time)
+        document.querySelector("#charge").classList.add("charge")
+    }, 1000);
+    setTimeout(() => {
+        document.querySelector("#charge").classList.add("charges")
+    }, 2000);
+    setTimeout(() => {
+        window.location.href="./pages/dashboard.html"
+    }, 3000);
+})
 
-
-setTimeout(() => {
-    clearInterval(time)
-    document.querySelector("#charge").classList.add("charge")
-}, 1000);
-setTimeout(() => {
-    // clearInterval(time)
-    document.querySelector("#charge").classList.add("charges")
-}, 2000);
 
 
 
